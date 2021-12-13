d10abb4d418a95392dbc97e82ef87e2060db08d0
边宇琨 <bianyukun1213@outlook.com>
2021-12-13 21:37:09 +0800

    update

--- tool.py
+++ tool.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import html
 import json
 import glob
 
@@ -14,7 +15,10 @@ for file_path in files:
     with open(file_path, 'r', encoding='utf-8') as f:
         data = f.read()
     loaded = json.loads(data)
-    for x, element in enumerate(loaded):
+    from_zhihuishu = False
+    if os.path.basename(file_path).find('zhihuishu') != -1:
+        from_zhihuishu = True
+    for element in loaded:
         question = {
             'index': index,
             'subject': os.path.splitext(file_path)[0].split('_')[1],
@@ -23,33 +27,57 @@ for file_path in files:
             'answer': ''
         }
         index += 1
-        if element['stlx'] == 'SC':
-            question['type'] = '单选'
-        elif element['stlx'] == 'MC':
-            question['type'] = '多选'
+        if from_zhihuishu == False:
+            if element['stlx'] == 'SC':
+                question['type'] = '单选'
+            elif element['stlx'] == 'MC':
+                question['type'] = '多选'
+            else:
+                question['type'] = '判断'
+            ansstr = element['da']
+            if 'A' in ansstr:
+                question['answer'] = ''.join(
+                    [question['answer'], element['xx1'][2:].strip()+'\n'])
+            if 'B' in ansstr:
+                question['answer'] = ''.join(
+                    [question['answer'], element['xx2'][2:].strip()+'\n'])
+            if 'C' in ansstr:
+                question['answer'] = ''.join(
+                    [question['answer'], element['xx3'][2:].strip()+'\n'])
+            if 'D' in ansstr:
+                question['answer'] = ''.join(
+                    [question['answer'], element['xx4'][2:].strip()+'\n'])
+            if 'E' in ansstr:
+                question['answer'] = ''.join(
+                    [question['answer'], element['xx5'][2:].strip()+'\n'])
+            if ansstr == '对' or ansstr == '错':
+                question['answer'] = ansstr
+            question['answer'] = question['answer'].strip()
+            question['title'] = element['wt']
+
         else:
-            question['type'] = '判断'
-        ansstr = element['da']
-        if 'A' in ansstr:
-            question['answer'] = ''.join(
-                [question['answer'], element['xx1'][2:].strip()+'\n'])
-        if 'B' in ansstr:
-            question['answer'] = ''.join(
-                [question['answer'], element['xx2'][2:].strip()+'\n'])
-        if 'C' in ansstr:
-            question['answer'] = ''.join(
-                [question['answer'], element['xx3'][2:].strip()+'\n'])
-        if 'D' in ansstr:
-            question['answer'] = ''.join(
-                [question['answer'], element['xx4'][2:].strip()+'\n'])
-        if 'E' in ansstr:
-            question['answer'] = ''.join(
-                [question['answer'], element['xx5'][2:].strip()+'\n'])
-        if ansstr == '对' or ansstr == '错':
-            question['answer'] = ansstr
-        question['answer'] = question['answer'].strip()
-        question['title'] = element['wt']
+            question['subject'] = os.path.splitext(file_path)[0].split('_')[2]
+            if element['questionTypeId'] == 1:
+                question['type'] = '单选'
+                for opt in element['optionList']:
+                    if opt['isCorrect'] == 1:
+                        question['answer'] = html.unescape(opt['content'])
+                        break
+            elif element['questionTypeId'] == 2:
+                question['type'] = '多选'
+                for opt in element['optionList']:
+                    if opt['isCorrect'] == 1:
+                        question['answer'] = ''.join(
+                            [question['answer'], html.unescape(opt['content'])+'\n'])
+                question['answer'] = question['answer'].strip()
+            else:
+                question['type'] = '判断'
+                if element['result'] == '0':
+                    question['answer'] = '错'
+                else:
+                    question['answer'] = '对'
+            question['title'] = html.unescape(element['content'])
         lst.append(question)
-final = json.dumps(lst, ensure_ascii=False)
+final = json.dumps(lst, ensure_ascii=False, indent=4)
 with open(script_path+'\data.json', 'w', encoding='utf-8') as a:
     a.write(final)
---
+++ tool_zhihuishu.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+import os
+import sys
+import time
+import json
+import requests
+
+LIST_URL = 'https://hike-examstu.zhihuishu.com/zhsathome/randomExercise/queryAnswerSheet?courseId=<COURSE_ID>&isFirst=true&randomExerciseStyle=0&uuid=<UUID>'
+DETAIL_URL = 'https://hike-examstu.zhihuishu.com/zhsathome/randomExercise/queryRandomExerciseDetail?courseId=<COURSE_ID>&questionId=<QUESTION_ID>&times=1&randomExerciseStyle=0&uuid=<UUID>'
+
+args = sys.argv
+course_id = ''
+uuid = ''
+if len(args) == 3:
+    course_id = args[1]
+    uuid = args[2]
+    list_url = LIST_URL.replace(
+        '<COURSE_ID>', course_id).replace('<UUID>', uuid)
+    exercises = requests.get(list_url).json()['rt']
+    exercise_count = exercises['randomExerciseCount']
+    exercise_list = exercises['lists']
+    print('获取到 %d 道习题。' % exercise_count)
+    details = []
+    for index, exercise in enumerate(exercise_list):
+        print('正在采集习题 %d……(%d/%d)' %
+              (exercise['exerciseId'], index + 1, exercise_count))
+        detail_url = DETAIL_URL.replace('<COURSE_ID>', course_id).replace(
+            '<UUID>', uuid).replace('<QUESTION_ID>', str(exercise['exerciseId']))
+        detail = requests.get(detail_url).json()['rt']
+        details.append(detail)
+        time.sleep(1)
+    print('采集完成，正在保存……')
+    save_path = os.path.split(os.path.realpath(sys.argv[0]))[
+        0] + '\\from_zhihuishu\\'
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    json_file_path = save_path + 'output_' + course_id + '.json'
+    json_file = open(json_file_path, mode='w', encoding='utf-8')
+    json.dump(details, json_file, ensure_ascii=False, indent=4)
+    json_file.close()
+    print('完成！')
+
+else:
+    print('参数错误！')
