01de6b1f43f61fd87eb9b1bdd014a3cf2d24e4c3
bianyukun1213 <bianyukun1213@outlook.com>
2023-05-05 18:04:13 +0800

    update

--- docker/scripts/run.sh
+++ docker/scripts/run.sh
@@ -4,6 +4,13 @@ usermod -u ${PUID} ma
 homedir="/home/ma"
 rm -rf ${homedir}
 mkdir ${homedir}
+
+chown ${PUID}:${PGID} -R ${homedir}
+chown ${PUID}:${PGID} -R /ma_data
+umask ${UMASK}
+
+su - ma
+
 if [ ${HTTP_PROXY} ]; then
    echo "HTTP_PROXY=${HTTP_PROXY}"
 	git config --global http.proxy ${HTTP_PROXY}
@@ -11,12 +18,11 @@ else
 	git config --global --unset http.proxy
 fi
 git clone https://github.com/bianyukun1213/MYZXKSAssistant.git ${homedir}
+
 if [ ${HTTP_PROXY} ]; then
    pip install --proxy ${HTTP_PROXY} -r ${homedir}/requirements.txt
 else
 	pip install -r ${homedir}/requirements.txt
 fi
-chown ${PUID}:${PGID} -R ${homedir}
-chown ${PUID}:${PGID} -R /ma_data
-umask ${UMASK}
-su - ma -c "python myzxks_assistant.py --container"
+
+python ${homedir}/myzxks_assistant.py --container
--- myzxks_assistant.py
+++ myzxks_assistant.py
@@ -7,6 +7,7 @@ import json
 from fuzzywuzzy import process
 import flask
 from flask import request
+from loguru import logger
 
 app = flask.Flask(__name__)
 
@@ -16,15 +17,15 @@ def search():
         arg_title = request.args['title'].strip()
         if arg_title == '':
             return '缺失 title！', 404, [('Content-Type', 'text/plain; charset=utf-8')]
-        print('收到格式正确的请求！', file=sys.stdout)
-        print('---\n参数中的题目：\n%s' % arg_title, file=sys.stdout)
+        logger.info('收到格式正确的请求！')
+        logger.info('参数中的题目：%s' % arg_title)
         global titles
         global answers
         result = process.extractOne(arg_title, titles)
         match_title = result[0]
         match_ratio = result[1]
-        print('匹配到的题目：\n%s' % match_title, file=sys.stdout)
-        print('匹配率：\n%d%%' % match_ratio, file=sys.stdout)
+        logger.info('匹配到的题目：%s' % match_title)
+        logger.info('匹配率：%d%%' % match_ratio)
         for i, element_answer in enumerate(answers):
             for j, element_title in enumerate(titles):
                 if element_title == match_title:
@@ -32,24 +33,27 @@ def search():
             if i == j:
                 answer = element_answer
                 break
-        print('答案：\n%s\n---' % answer, file=sys.stdout)
+        logger.info('答案：%s' % answer.replace('\n','；'))
         return ''.join([match_title, '\n---\n', answer]), 200, [('Content-Type', 'text/plain; charset=utf-8')]
     return '缺失 title！', 404, [('Content-Type', 'text/plain; charset=utf-8')]
 
 if(__name__ == '__main__'):
-    print('欢迎使用 Hollis(his2nd.life) 的马院考试助手！', file=sys.stdout)
-    print('请访问 https://github.com/bianyukun1213/MYZXKSAssistant 阅读使用文档。', file=sys.stdout)
     data_path = '/ma_data/data.json'
+    log_path = '/ma_log'
     arg1 = ''
     if(len(sys.argv)>1):
         arg1 = sys.argv[1]
     if(arg1 != '--container'):
         data_path = os.path.split(os.path.realpath(sys.argv[0]))[0] + data_path
-    print('从 %s 读取数据。' % data_path, file=sys.stdout)
+        log_path = os.path.split(os.path.realpath(sys.argv[0]))[0] + log_path
+    logger.add(log_path + '/{time}.log', rotation = '00:00', retention = 14)
+    logger.info('欢迎使用 Hollis(his2nd.life) 的马院考试助手！')
+    logger.info('请访问 https://github.com/bianyukun1213/MYZXKSAssistant 阅读使用文档。')
+    logger.info('arg1: %s, data_path: %s, log_path: %s' % (arg1, data_path, log_path))
     if(os.path.isfile(data_path) != True):
-        print('数据文件不存在！', file=sys.stdout)
+        logger.info('数据文件不存在！')
         sys.exit()
-    with open(data_path, 'r', encoding='utf-8') as f:
+    with open(data_path, 'r', encoding = 'utf-8') as f:
         data = f.read()
     loaded = json.loads(data)
     titles = []
@@ -57,5 +61,5 @@ if(__name__ == '__main__'):
     for question in loaded:
         titles.append(question['title'])
         answers.append(question['answer'])
-    print('%s 条数据已加载，即将运行服务。' % len(loaded), file=sys.stdout)
-    app.run(host='0.0.0.0', port=8972)
+    logger.info('%s 条数据已加载，即将运行服务。' % len(loaded))
+    app.run(host = '0.0.0.0', port = 8972)
